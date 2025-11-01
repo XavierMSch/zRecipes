7e47298c0ad7554113e03f915944e2011cc6f4f5
Xavier <138932973+XavierMSch@users.noreply.github.com>
2025-11-01 13:19:33 -0300

    implemented database conection

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,22 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import re
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from typing import AsyncGenerator
 
-SQL_ALCHEMY_DB_URL = "postgresql://[USER]:[PASSWORD]@localhost:5432/[DB_NAME]"
+DATABASE_URL = 'postgresql://neondb_owner:npg_iOz3GWxpyTL6@ep-cold-tree-acyfan2v-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 
-engine = create_engine(SQL_ALCHEMY_DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+engine = create_async_engine(re.sub(r'^postgresql:', 'postgresql+asyncpg:', DATABASE_URL), echo=True)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
