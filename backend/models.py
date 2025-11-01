9d309a68544cec63d9b76c5b684df71e63ec19ea
Xavier <138932973+XavierMSch@users.noreply.github.com>
2025-11-01 16:31:01 -0300

    added likes and reports to db models

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,15 @@
-from sqlalchemy import ForeignKey, Integer, Identity, String, Boolean, Table, Column
+from sqlalchemy import ForeignKey, Integer, Identity, String, Boolean, Table, Column, DateTime, func
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 from database import Base
+
+user_likes_association = Table(
+    "user_likes_association",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("recipe_id", ForeignKey("recipes.id"), primary_key=True),
+)
 
 class User(Base):
     __tablename__ = "users"
@@ -15,7 +23,15 @@ class User(Base):
     is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)    
 
     recipes: Mapped[list["Recipe"]] = relationship(back_populates="owner")
+
     recipe_lists: Mapped[list["RecipeList"]] = relationship(back_populates="owner")
+
+    liked_recipes: Mapped[list["Recipe"]] = relationship(
+        secondary=user_likes_association,
+        back_populates="liked_by_users",
+    )
+
+    reports: Mapped[list["Report"]] = relationship(back_populates="reporter")
 
 recipe_list_association = Table(
     "recipe_list_association",
@@ -24,6 +40,17 @@ recipe_list_association = Table(
     Column("recipe_list_id", ForeignKey("recipe_lists.id"), primary_key=True),
 )
 
+class Report(Base):
+    __tablename__ = "reports"
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
+
+    reporter_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    reporter: Mapped["User"] = relationship(back_populates="reports")
+
+    recipe_id: Mapped[int] = mapped_column(ForeignKey("recipes.id"))
+    recipe: Mapped["Recipe"] = relationship(back_populates="reports")
+
 class Recipe(Base):
     __tablename__ = "recipes"
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True, index=True)
@@ -31,14 +58,21 @@ class Recipe(Base):
     image_url: Mapped[str] = mapped_column(String(1024), nullable=True)
     ingredients: Mapped[list[dict[str, str]]] = mapped_column(JSONB, nullable=False, server_default='[]')
     steps: Mapped[list[dict[str, str]]] = mapped_column(JSONB, nullable=False, server_default='[]')
-    likes: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
     owner: Mapped["User"] = relationship(back_populates="recipes")
+
     recipe_lists: Mapped[list["RecipeList"]] = relationship(
         secondary=recipe_list_association,
         back_populates="recipes",
     )
+
+    liked_by_users: Mapped[list["User"]] = relationship(
+        secondary=user_likes_association,
+        back_populates="liked_recipes",
+    )
+    
+    reports: Mapped[list["Report"]] = relationship(back_populates="recipe")
 
 class RecipeList(Base):
     __tablename__ = "recipe_lists"
