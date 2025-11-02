31072551f4086b469865e5998d012d3b422eeca6
Xavier <138932973+XavierMSch@users.noreply.github.com>
2025-11-02 10:40:48 -0300

    get user by id function created

--- backend/crud.py
+++ backend/crud.py
@@ -1,2 +1,8 @@
-async def get_user_by_id(db, id):
-    pass
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from . import models
+
+async def get_user_by_id(db: AsyncSession, id: int) -> models.User | None:
+    # Busca un usuario por id
+    result = await db.execute(select(models.User).filter(models.User.id == id))
+    return result.scalar_one_or_none()
