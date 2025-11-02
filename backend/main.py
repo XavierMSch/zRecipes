34654253c0d8422a4d04ff84995de64a26f23db8
Xavier <138932973+XavierMSch@users.noreply.github.com>
2025-11-02 20:36:08 -0300

    Added global exception handler

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import auth, recipes, interactions, recipe_lists
@@ -21,6 +22,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"message": "Error interno del servidor."},
+    )
 
 app.include_router(auth.router)
 app.include_router(recipes.router)
